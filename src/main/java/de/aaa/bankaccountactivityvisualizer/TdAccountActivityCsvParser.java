fedff640dd0e7e9f655013256bd6b18ff242e832
HerrAachen <MalikAtalla@hotmail.com>
2015-07-15 23:07:44 -0700

    ...

--- src/main/java/de/aaa/bankaccountactivityvisualizer/TdAccountActivityCsvParser.java
+++ src/main/java/de/aaa/bankaccountactivityvisualizer/TdAccountActivityCsvParser.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
@@ -21,6 +24,11 @@ public class TdAccountActivityCsvParser {
   private static final int DATE_COLUMN = 0;
   private static final String DELIMITER = ",";
   DateTimeFormatter dateFormat = DateTimeFormat.forPattern("MM/dd/yyyy");
+  
+  public AccountActivity parseAccountActivity(String directory) throws IOException{
+    Stream<Path> filesInDirectory = Files.list(new File(directory).toPath());
+    Stream<Path> csvFiles = filesInDirectory.filter(p -> p.toString().endsWith(".csv"));
+  }
 
   public AccountActivity parseAccountActivity(File file) throws IOException {
     if (file == null) {
